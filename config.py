53cbb0645ac321388b7a7545b3724df9c077d832
FrankSkep <f.cornejo.pacheco@gmail.com>
2025-02-03 18:46:04 -0800

    add and use config.py, add requirements

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
-import os
-from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
 
-load_dotenv()
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    SECRET_KEY: str
+    DEBUG: bool = False
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
-class Settings:
-    DATABASE_URL = os.getenv("DATABASE_URL")
+    class Config:
+        env_file = ".env"
 
 settings = Settings()
